241f6432eb893a912eb7c18db438609fb95f130e
zoya5636 <zoya@chishtie.com>
2025-02-09 21:51:58 -0500

    added config and function

--- bookmanagement/views.py
+++ bookmanagement/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
 
-# Create your views here.
+def search_for_books(request):
+    user_query = request.GET.get("q", "")
+    user_query = user_query.replace(" ", "+")
+    url = f"{settings.API_BASE_URL}/volumes/q={user_query}&maxResults={settings.API_MAX_RESULTS}&key={settings.API_API_KEY}"
+
+    resp = requests.get(url)
+    books = resp.json()["items"]
